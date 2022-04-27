926a39c7d13ac51e7b2590867d2665bc39b576ec
Árpád Goretity <h2co3@h2co3.org>
2022-04-27 08:56:41 +0200

    Version 0.1.1: update documentation because `CStr::from_bytes_with_nul_unchecked()` is now `const`; use `quote_spanned!()` and `syn::Error` instead of panicking; update dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,15 @@
 //! Zero-terminated C string literals.
-#![doc(html_root_url = "https://docs.rs/neodyn_db/0.1.0")]
 
 use proc_macro::TokenStream;
-use syn::{ parse_macro_input, Lit, LitByteStr };
-use quote::quote;
+use proc_macro2::TokenStream as TokenStream2;
+use syn::{ parse2, Error, Lit, LitByteStr };
+use quote::quote_spanned;
 
 /// Given a Rust string or byte string literal, this macro
 /// generates an expression of type `&'static CStr` that is
 /// properly 0-terminated and ensured not to contain any
-/// internal NUL (0) bytes. The conversion is zero-cost and
-/// it may even become `const` in the future, provided that
-/// `CStr::from_bytes_with_nul_unchecked()` becomes `const`.
+/// internal NUL (0) bytes. The conversion is zero-cost, and
+/// the resulting expression can be used in `const` context.
 ///
 /// ### Examples:
 ///
@@ -38,22 +37,31 @@ use quote::quote;
 /// let invalid_1 = zstr!("null here: \x00 is forbidden");
 /// let invalid_2 = zstr!("also at the end: \0");
 /// let invalid_3 = zstr!(b"and in byte \x00 strings too");
+/// let invalid_4 = zstr!(b"at the end of byte strings: \0");
 /// ```
 #[proc_macro]
 pub fn zstr(input: TokenStream) -> TokenStream {
-    let literal = parse_macro_input!(input as Lit);
+    expand_zstr(input.into())
+        .unwrap_or_else(Error::into_compile_error)
+        .into()
+}
+
+/// Performs the actual expansion of `zstr!()`.
+fn expand_zstr(input: TokenStream2) -> Result<TokenStream2, Error> {
+    let literal: Lit = parse2(input)?;
     let span = literal.span();
 
     let mut bytes = match literal {
         Lit::Str(lit) => lit.value().into_bytes(),
         Lit::ByteStr(lit) => lit.value(),
-        _ => panic!("expected a string or byte string literal"),
+        _ => return Err(Error::new(span, "expected a string or byte string literal")),
     };
 
     // Ensure that no 0 byte is in the string literal, as that
     // would cause inconsistencies in the length of the string.
     if let Some(index) = bytes.iter().position(|&b| b == 0x00) {
-        panic!("C string contains an embedded NUL byte at index {}", index);
+        let message = format!("C string contains an embedded NUL byte at index {}", index);
+        return Err(Error::new(span, message));
     }
 
     // Add the terminating 0.
@@ -64,13 +72,11 @@ pub fn zstr(input: TokenStream) -> TokenStream {
     let bstr = LitByteStr::new(&bytes, span);
 
     // Expand to an expression of type `&'static CStr`.
-    let expanded = quote!{
+    Ok(quote_spanned!{
         // SAFETY: the input is NUL-terminated and it is ensured
         // that it does not contain any other, internal NUL bytes.
-        unsafe {
+        span => unsafe {
             ::std::ffi::CStr::from_bytes_with_nul_unchecked(#bstr)
         }
-    };
-
-    TokenStream::from(expanded)
+    })
 }
